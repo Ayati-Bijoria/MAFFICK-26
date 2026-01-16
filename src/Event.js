018702f156a7140fdefb53e963c196cfcb0aedcc
Ayati-Bijoria <bijoriaayati@gmail.com>
2026-01-16 19:49:52 +0530

    events and comps

--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,15 @@
 import App3 from "./App3";
 import './Event.css';
 import deshaj from './img/deshaj.jpeg';
+import beatstorm from './img/Beat_storm.jpeg';
+import anveshan from './img/Anvesham.jpeg';
 function App() {
   return (
     <>
       <div className="flip-container">
         <div className="flip-row">
-          <App3 imgsrc={deshaj} front="deshaj 1.0" back="24 01"/>
-          <App3 imgsrc={deshaj} front="deshaj 1.0" back="24 01"/>
+          <App3 imgsrc={beatstorm} front="BEAT STORM" back="ROBAROO 16 Jan 1:15pm-1:30pm" />
+          <App3 imgsrc={anveshan} front="ANVESHAN-treasure HUNT" back="ROBOTICS CLUN 23-25Jan LRC"/>
           <App3 imgsrc={deshaj} front="deshaj 1.0" back="24 01"/>
         </div>
       </div>
